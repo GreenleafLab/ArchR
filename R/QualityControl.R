2af6340a871a420a923f477a23c7eff50167b922
jgranja24 <jgranja@stanford.edu>
2020-03-10 00:49:48 -0700

    bugfix

--- R/QualityControl.R
+++ R/QualityControl.R
@@ -89,7 +89,7 @@ plotTSSEnrichment <- function(
   }else{
 
     plotDF <- data.frame(x=dfTSS$x,v=dfTSS$smoothValue,sampleName=dfTSS$sampleName)
-    plotDF <- plotDF[sort(unique(c(1,seq_len(1,nrow(plotDF),11),nrow(plotDF)))), , drop = FALSE]
+    plotDF <- plotDF[sort(unique(c(1,seq(1,nrow(plotDF),11),nrow(plotDF)))), , drop = FALSE]
     
     p <- ggplot(plotDF, aes(x,v,color=sampleName)) +
       geom_line(size = 1) +
