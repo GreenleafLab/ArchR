73287e62b2340ec9e3c9ed62c76c16820e417826
jgranja24 <jgranja@stanford.edu>
2021-04-11 21:29:26 -0700

    bug fix sampledCellNames arrowRead

--- R/ArrowRead.R
+++ R/ArrowRead.R
@@ -784,7 +784,7 @@ getMatrixFromArrow <- function(
 
     matFiles <- lapply(mat, function(x) x[[2]]) %>% Reduce("c", .)
     mat <- lapply(mat, function(x) x[[1]]) %>% Reduce("cbind", .)
-    if(!all(cellNames %in% colnames(mat))){
+    if(!all(sampledCellNames %in% colnames(mat))){
       .logThis(sampledCellNames, "cellNames supplied", logFile = logFile)
       .logThis(colnames(mat), "cellNames from matrix", logFile = logFile)
       stop("Error not all cellNames found in partialMatrix")
