75544172bc1962fd0050bb190450ee826ce1aba0
Ryan Corces <ryancorces@gmail.com>
2021-11-01 21:24:43 -0700

    fix logic of if statement after partial matrix sampling
    in response to https://github.com/GreenleafLab/ArchR/issues/1130

--- R/ArrowRead.R
+++ R/ArrowRead.R
@@ -783,10 +783,10 @@ getMatrixFromArrow <- function(
 
     matFiles <- lapply(mat, function(x) x[[2]]) %>% Reduce("c", .)
     mat <- lapply(mat, function(x) x[[1]]) %>% Reduce("cbind", .)
-    if(!all(cellNames %in% colnames(mat))){
+    if(!all(colnames(mat) %in% cellNames)){
       .logThis(sampledCellNames, "cellNames supplied", logFile = logFile)
       .logThis(colnames(mat), "cellNames from matrix", logFile = logFile)
-      stop("Error not all cellNames found in partialMatrix")
+      stop("Error - some colnames in partialMatrix do not match cellNames")
     }
     mat <- mat[,sampledCellNames, drop = FALSE]
     mat <- .checkSparseMatrix(mat, length(sampledCellNames))
