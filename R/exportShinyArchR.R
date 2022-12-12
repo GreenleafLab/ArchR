2567e09c4303dff5b3012ba8c330e6f2c28693cb
Paulina Paiz <paulinapaiz02@gmail.com>
2022-12-11 18:25:05 -0800

    cleaning up

--- R/exportShinyArchR.R
+++ R/exportShinyArchR.R
@@ -1,216 +1,3 @@
-# Setting up ----------------------------------------------------------------------
-
-library(shinycssloaders)
-library(hexbin)
-library(magick)
-library(gridExtra)
-library(grid)
-library(patchwork)
-library(shinybusy)
-library(cowplot)
-library(ggpubr)
-library(farver)
-library(rhdf5)
-library(plotfunctions)
-library(raster)
-library(jpeg)
-library(sparseMatrixStats)
-library(BiocManager)
-# options(repos = BiocManager::repositories())
-library(AnnotationDbi)
-library(BSgenome)
-library(Biobase)
-library(BiocGenerics)
-library(BiocParallel)
-library(Biostrings)
-library(CNEr)
-library(ComplexHeatmap)
-# options(download.file.method = "libcurl")
-# devtools::install_github("selcukorkmaz/ArchR", ref = "dev")
-library(ArchR)
-
-# specify whether you use a local machine or the shiny app
-ShinyArchR = TRUE
-
-# specify desired number of threads 
-addArchRThreads(threads = 1) 
-# specify genome version. Default hg19 set
-addArchRGenome("hg19")
-set.seed(1)
-
-ArchRProj=loadArchRProject(path = "Save-ProjHeme5/")
-ArchRProj <- addImputeWeights(ArchRProj = ArchRProj)
-setwd(getOutputDirectory(ArchRProj))
-
-# myLoadArchRProject -----------------------------------
-#' Load Previous ArchRProject into R
-#'
-#' This function will load a previously saved ArchRProject and re-normalize paths for usage.
-#'
-#' @param path A character path to an `ArchRProject` directory that was previously saved using `saveArchRProject()`.
-#' @param force A boolean value indicating whether missing optional `ArchRProject` components (i.e. peak annotations /
-#' background peaks) should be ignored when re-normalizing file paths. If set to `FALSE` loading of the `ArchRProject`
-#' will fail unless all components can be found.
-#' @param showLogo A boolean value indicating whether to show the ascii ArchR logo after successful creation of an `ArchRProject`.
-#' @export
-myLoadArchRProject <- function(path = "./",
-                               force = FALSE,
-                               showLogo = TRUE) {
-  .validInput(input = path,
-              name = "path",
-              valid = "character")
-  .validInput(input = force,
-              name = "force",
-              valid = "boolean")
-  .validInput(input = showLogo,
-              name = "showLogo",
-              valid = "boolean")
-  
-  path2Proj <- file.path(path, "Save-ArchR-Project.rds")
-  
-  if (!file.exists(path2Proj)) {
-    stop("Could not find previously saved ArchRProject in the path specified!")
-  }
-  
-  ArchRProj <- recoverArchRProject(readRDS(path2Proj))
-  outputDir <- getOutputDirectory(ArchRProj)
-  outputDirNew <- normalizePath(path)
-  
-  
-  ArchRProj@projectMetadata$outputDirectory <- outputDirNew
-  
-  message("Successfully loaded ArchRProject!")
-  if (showLogo) {
-    .ArchRLogo(ascii = "Logo")
-  }
-  
-  ArchRProj
-  
-}
-
-
-## Create fragment files -----------------------------------------------------------
-.getGroupFragsFromProj <- function(ArchRProj = NULL,
-                                   groupBy = NULL,
-                                   outDir = file.path("Shiny", "fragments")) {
-  dir.create(outDir, showWarnings = FALSE)
-  
-  # find barcodes of cells in that groupBy.
-  groups <- getCellColData(ArchRProj, select = groupBy, drop = TRUE)
-  cells <- ArchRProj$cellNames
-  cellGroups <- split(cells, groups)
-  
-  # outputs unique cell groups/clusters.
-  clusters <- names(cellGroups)
-  
-  
-  for (cluster in clusters) {
-    cat("Making fragment file for cluster:", cluster, "\n")
-    # get GRanges with all fragments for that cluster
-    cellNames = cellGroups[[cluster]]
-    fragments <-
-      getFragmentsFromProject(ArchRProj = ArchRProj, cellNames = cellNames)
-    fragments <- unlist(fragments, use.names = FALSE)
-    # filter Fragments
-    fragments <-
-      GenomeInfoDb::keepStandardChromosomes(fragments, pruning.mode = "coarse")
-    saveRDS(fragments, file.path(outDir, paste0(cluster, "_cvg.rds")))
-  }
-}
-
-addSeqLengths <- function (gr, genome) {
-  gr <- ArchR:::.validGRanges(gr)
-  genome <- validBSgenome(genome)
-  stopifnot(all(as.character(seqnames(gr)) %in% as.character(seqnames(genome))))
-  seqlengths(gr) <-
-    seqlengths(genome)[as.character(names(seqlengths(gr)))]
-  return(gr)
-}
-
-.getClusterCoverage <- function(ArchRProj = NULL,
-                                tileSize = 100,
-                                scaleFactor = 1,
-                                groupBy = "Clusters",
-                                outDir = file.path("Shiny", "coverage")) {
-  fragfiles = list.files(path = file.path("Shiny", "fragments"),
-                         full.names = TRUE)
-  dir.create(outDir, showWarnings = FALSE)
-  
-  # find barcodes of cells in that groupBy.
-  groups <- getCellColData(ArchRProj, select = groupBy, drop = TRUE)
-  cells <- ArchRProj$cellNames
-  cellGroups <- split(cells, groups)
-  
-  # outputs unique cell groups/clusters.
-  clusters <- names(cellGroups)
-  
-  chrRegions <- getChromSizes(ArchRProj)
-  genome <- getGenome(ArchRProj)
-  
-  for (file in fragfiles) {
-    fragments <- readRDS(file)
-    #fragmentsToInsertions()
-    left <- GRanges(seqnames = seqnames(fragments),
-                    ranges = IRanges(start(fragments), width = 1))
-    right <- GRanges(seqnames = seqnames(fragments),
-                     ranges = IRanges(end(fragments), width = 1))
-    # call sort() after sortSeqlevels() to sort also the ranges in addition
-    # to the chromosomes.
-    insertions <- c(left, right) %>% sortSeqlevels() %>%
-      sort()
-    
-    cluster <- file %>% basename() %>% gsub("_.*", "", .)
-    #binnedCoverage
-    # message("Creating bins for cluster ",clusters[clusteridx], "...")
-    bins <-
-      unlist(slidingWindows(chrRegions, width = tileSize, step = tileSize))
-    # message("Counting overlaps for cluster ",clusters[clusteridx], "...")
-    bins$reads <-
-      countOverlaps(
-        bins,
-        insertions,
-        maxgap = -1L,
-        minoverlap = 0L,
-        type = "any"
-      )
-    addSeqLengths(bins, genome)
-    # message("Creating binned coverage for cluster ",clusters[clusteridx], "...")
-    #each value is multiplied by that weight.
-    # TODO add scaleFactor
-    # allCells as.vector(ArchRProj@cellColData$Sample, mode="any")
-    clusterReadsInTSS <-
-      ArchRProj@cellColData$ReadsInTSS[cells %in% cellGroups$cluster]
-    # scaleFactor <- 5e+06 / sum(clusterReadsInTSS)
-    binnedCoverage <-
-      coverage(bins, weight = bins$reads * scaleFactor)
-    saveRDS(binnedCoverage, file.path(outDir, paste0(cluster, "_cvg.rds")))
-  }
-  
-}
-
-
-#############################################################
-
-# ArchRProj=myLoadArchRProject("./Shiny/inputData/")
-
-
-# Load all hidden ArchR functions ------------------------------------------------
-fn <- unclass(lsf.str(envir = asNamespace("ArchR"), all = TRUE))
-for (i in seq_along(fn)) {
-  tryCatch({
-    # eval(parse(text = paste0(fn[i], "<-", fn[i])))
-    eval(parse(text=paste0(fn[i], '<-ArchR:::', fn[i])))
-  }, error = function(x) {
-  })
-}
-
-
-outputDir = "Shiny"
-subOutputDir = "inputData"
-
-
-
-
 # exportShiny function -----------------------------------------------------------
 #' Export a Shiny App based on ArchRProj
 #' 
@@ -248,11 +35,7 @@ exportShinyArchR <- function(
   
   .requirePackage("shiny", installInfo = 'install.packages("shiny")')
   .requirePackage("rhandsontable", installInfo = 'install.packages("rhandsontable")')
-  
-  # ArchRProj <- myLoadArchRProject(path = paste0("./",outputDir,"/inputData/"))
-  # ArchRProj <- addImputeWeights(ArchRProj = ArchRProj)
 
-  # TODO: Check that all columns exist in cellColData 
   if(is.null(groupBy)){
     stop("groupBy must be provided")
   } else if(groupBy %ni% colnames(getCellColData(ArchRProj))){
@@ -332,64 +115,16 @@ exportShinyArchR <- function(
     message("Coverage files already exist...")
     
   }
-  ## main umaps -----------------------------------------------------------------
 
   dir.create(file.path(getOutputDirectory(ArchRProj), outputDir, subOutputDir),showWarnings = TRUE)
 
-  # need arrowFiles to getFeatures so need to save genes as RDS
-  # TODO change hardcoding of these paths: Should be file.path(getOutputDirectory(ArchRProj), outputDir, subOutputDir, "features.rds"
-  if(!file.exists(file.path(getOutputDirectory(ArchRProj), outputDir, subOutputDir, "gene_names.rds"))){
-    # TODO probably need to add param to get specific features based on matrix?
-    gene_names <- getFeatures(ArchRProj = ArchRProj)
-    saveRDS(gene_names, paste0("./", outputDir, "/", subOutputDir,"/gene_names.rds"))
-  }else{
-    message("gene_names already exists...")
-    gene_names <- readRDS(paste0("./", outputDir, "/", subOutputDir,"/gene_names.rds"))
-  }
-  
-  if(!file.exists(paste0("./", outputDir, "/", subOutputDir,"/umaps.rds"))){  
-    umaps <- list()
-    umapNames <- colnames(ArchRProjShiny@cellColData)
-    
-    for(x in 1:length(umapNames)){
-      
-      print(umapNames[x])
-      
-      tryCatch(
-        umap <- plotEmbedding(
-          ArchRProj = ArchRProjShiny,
-          baseSize=12,
-          colorBy = "cellColData",
-          name = umapNames[x],
-          embedding = embedding,
-          rastr = FALSE,
-          size=0.5,
-        )+ggtitle("Colored by scATAC-seq clusters")+theme(text=element_text(size=12),
-                                                          legend.title = element_text(size = 12),legend.text = element_text(size = 6)),
-        
-        umaps[[umapNames[[x]]]] <- umap,
-        error = function(e){
-        print(e)
-        })
-    }
-    
-    saveRDS(umaps, paste0("./", outputDir, "/", subOutputDir,"/umaps.rds"))
-    
-    
-    }else{
-      message("umaps already exists...")
-      umaps <- readRDS(paste0("./", outputDir, "/", subOutputDir,"/umaps.rds"))
-    }
-    
-    
   ## colorMats without Impute Weights----------------------------------------------------------------
   
-  #TODO check with matrices are available
+  # check which matrices are available
   allMatrices <- getAvailableMatrices(ArchRProj)
 
-  #Get gene and motif names and save as RDS
+  # Get gene and motif names and save as RDS
   if(!file.exists(paste0("./", outputDir, "/", subOutputDir,"/gene_names_GSM.rds"))){
-    # TODO check if ArchRProj has GSM
     if ("GeneScoreMatrix" %in% allMatrices){
       gene_names_GSM <- getFeatures(ArchRProj = ArchRProj, useMatrix = "GeneScoreMatrix")
       saveRDS(gene_names_GSM, paste0("./", outputDir, "/", subOutputDir,"/gene_names_GSM.rds"))
@@ -403,7 +138,6 @@ exportShinyArchR <- function(
   
   
 if(!file.exists(paste0("./", outputDir, "/", subOutputDir,"/gene_names_GIM.rds"))){
-  # TODO check if ArchRProj has GIM
   if ("GeneIntegrationMatrix" %in% allMatrices){
     gene_names_GIM <- getFeatures(ArchRProj = ArchRProj, useMatrix = "GeneIntegrationMatrix")
     saveRDS(gene_names_GSM, paste0("./", outputDir, "/", subOutputDir,"/gene_names_GIM.rds"))
@@ -418,7 +152,6 @@ if(!file.exists(paste0("./", outputDir, "/", subOutputDir,"/gene_names_GIM.rds")
   
 
   if(!file.exists(paste0("./", outputDir, "/", subOutputDir,"/motif_names.rds"))){
-     # TODO check if ArchRProj has MM
     if ("MotifMatrix" %in% allMatrices){
     motif_names <- getFeatures(ArchRProj = ArchRProj, useMatrix = "MotifMatrix") %>% 
       gsub(".*:", "", .) %>% unique(.)
@@ -456,7 +189,6 @@ if(!file.exists(paste0("./", outputDir, "/", subOutputDir,"/gene_names_GIM.rds")
     ),sparse = TRUE)
     matrices$"GeneIntegrationMatrix" <- colorMatGIM
     
-    #colorMatMM has 1740 rows because in name = getFeatures() returns the 870 z: + the 870 deviations:
     colorMatMM <- Matrix(.getMatrixValues(
       ArchRProj = ArchRProj,
       #name = getFeatures(ArchRProj, "MotifMatrix")
@@ -467,9 +199,7 @@ if(!file.exists(paste0("./", outputDir, "/", subOutputDir,"/gene_names_GIM.rds")
     ), sparse = TRUE)
     matrices$"MotifMatrix" <- colorMatMM
     
-    #TODO modify this so it only has the matrices we are actually supporting
-    matrices$allColorBy=c("colData", "cellColData", .availableArrays(head(getArrowFiles(ArchRProj), 2)))
-    # shouldn't save rds because it's too hefty for ShinyApps
+    matrices$allColorBy=c(.availableArrays(head(getArrowFiles(ArchRProj), 2)))
     saveRDS(matrices, paste0("./", outputDir, "/", subOutputDir,"/matrices.rds"))
     
   }else{
