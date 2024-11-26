59fee908d82ad3ffa865a0318ac97f4037cd1a46
Immanuel Abdi <immanuelazn@gmail.com>
2024-11-26 07:01:18 +0000

    fix issues with creating Seuratv5 obj

--- R/RNAIntegration.R
+++ R/RNAIntegration.R
@@ -472,7 +472,10 @@ addGeneIntegrationMatrix <- function(
       o <- suppressWarnings(file.remove(unlist(getImputeWeights(subProj)[[1]]))) #Clean Up Space
       .logThis(mat, paste0("GeneScoreMat-Block-Impute-",i), logFile=logFile)
     }
-
+    # Seurat v5 requires character type row/colnames
+    # sparse matrices occasionally convert names to array type, so convert back to create seurat obj
+    rownames(mat) <- as.character(rownames(mat))
+    colnames(mat) <- as.character(colnames(mat))
     #Log-Normalize 
     mat <- log(mat + 1) #use natural log
     seuratATAC <- Seurat::CreateSeuratObject(counts = mat[head(seq_len(nrow(mat)), 5), , drop = FALSE])
