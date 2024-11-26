8462492a38c0a42beab54d5d72be6f3cbe3055ab
Immanuel Abdi <56730419+immanuelazn@users.noreply.github.com>
2024-11-26 14:13:34 -0800

    Ia/fix lapply plot bug (#2240)
    * fix suppresswarnings typo
    * fix legend plotting lapply on R<4.3

--- R/VisualizeData.R
+++ R/VisualizeData.R
@@ -882,11 +882,14 @@ plotGroups <- function(
 
   #adapted from https://github.com/jwdink/egg/blob/master/R/set_panel_size.r
   g <- ggplotGrob(p)
-  
   legend_indices <- grep("guide-box", g$layout$name)
   # ggplot versions > 3.5 can have multiple guide boxes, and are non automatically removed if is a "zeroGrob"
   # Do filtering wrt only non "zeroGrob" legends
-  legend <- legend_indices[sapply(legend_indices, function(idx) class(g$grobs[[idx]])[1] != "zeroGrob")]
+  if (length(legend_indices) != 0) {
+    legend <- legend_indices[unlist(lapply(legend_indices, function(idx) class(g$grobs[[idx]])[1] != "zeroGrob"))]
+  } else {
+    legend <- legend_indices
+  }
   # add correct ones to legend
   if(length(legend)!=0){
     gl <- g$grobs[[legend]]
